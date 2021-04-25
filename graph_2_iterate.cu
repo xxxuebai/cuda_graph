890e8bdc58ed54e01adfb404be7b136343e691a7
xxxuebai <77400176+xxxuebai@users.noreply.github.com>
2021-04-25 17:02:42 +0800

    Update graph_2_iterate.cu

--- graph_2_iterate.cu
+++ graph_2_iterate.cu
@@ -4,58 +4,48 @@
 #include<math.h>
 
 const int N = 1024 * 1024;
-const int GPUTHREADNUM = 256;
-const int GPUBLOCKNUM = 256; 
+const int THREAD = 256;
+const int BLOCK = 256; 
 __global__ void kernelA(int *a,int *b,int *c)
 {
 	int idx = blockIdx.x * blockDim.x + threadIdx.x;
 	if(idx < N)
 	{
 		c[idx] = (a[idx] + b[idx])/2;
+                a[idx] = a[idx] + 1;
+                b[idx] = b[idx] + 1;
 	}
 }
 
-__global__ void kernelB(int *c,int *r,int *a)
-{
-	int idx = blockIdx.x * blockDim.x + threadIdx.x;
-	if(idx < N)
-	{
-		r[idx] = (c[idx] +a[idx]) /2;
-	}
-}
-void cudaGraphSample(int *dev_a,int *dev_b,int *dev_c,int *dev_r)
+void cudaGraphSample(int *dev_a,int *dev_b,int *dev_c)
 {
 	cudaStream_t stream0;
 	cudaGraph_t graph;
 	cudaStreamCreate(&stream0);
-//	cudaStreamCreate(&streamForGraph);
 	cudaStreamBeginCapture(stream0);
-	kernelA << <N/GPUBLOCKNUM , GPUTHREADNUM,0,stream0 >> >(dev_a,dev_b,dev_c);
-        kernelB << <N/GPUBLOCKNUM,GPUTHREADNUM,0,stream0 >> >(dev_c,dev_r,dev_a);
+	kernelA << <N/BLOCK,THREAD,0,stream0 >> >(dev_a,dev_b,dev_c);
+        kernelA << <N/BLOCK,THREAD,0,stream0 >> >(dev_a,dev_b,dev_c);
 	cudaStreamEndCapture(stream0,&graph);
 	cudaGraphExec_t graphExec;
 	cudaGraphInstantiate(&graphExec,graph,NULL,NULL,0);
 	for(int i=0;i<100;i++)
 	{
 		cudaGraphLaunch(graphExec,stream0);
-	
 	}
 	cudaStreamSynchronize(stream0);
 	cudaGraphExecDestroy(graphExec);
 	cudaGraphDestroy(graph);
-	//cudaStreamDestroy(streamForGraph);
 	cudaStreamDestroy(stream0);
 }
 
 int main()
 {
 	int *host_a,*host_b,*host_c;
-	int *dev_a,*dev_b,*dev_c,*dev_r;
+	int *dev_a,*dev_b,*dev_c;
 	int i;
 	cudaMalloc((void**)&dev_a,N*sizeof(int));
 	cudaMalloc((void**)&dev_b,N*sizeof(int));
 	cudaMalloc((void**)&dev_c,N*sizeof(int));
-	cudaMalloc((void**)&dev_r,N*sizeof(int));
 	cudaHostAlloc((void **)&host_a,N * sizeof(int),cudaHostAllocDefault);
 	cudaHostAlloc((void **)&host_b,N * sizeof(int),cudaHostAllocDefault);
 	cudaHostAlloc((void **)&host_c,N * sizeof(int),cudaHostAllocDefault);
@@ -67,10 +57,7 @@ int main()
 	cudaMemcpyAsync(dev_a,host_a,N * sizeof(int),cudaMemcpyHostToDevice);
  	cudaMemcpyAsync(dev_b,host_b,N * sizeof(int),cudaMemcpyHostToDevice);
 	cudaDeviceSynchronize();
-	
-	
-		cudaGraphSample(dev_a,dev_b,dev_c,dev_r);
-	
+	cudaGraphSample(dev_a,dev_b,dev_c);
 	cudaMemcpyAsync(host_c,dev_r,N * sizeof(int),cudaMemcpyDeviceToHost);
 	cudaDeviceSynchronize();
 	for(i=0;i<10;i++)
@@ -82,6 +69,5 @@ int main()
 	cudaFreeHost(host_c);
 	cudaFree(dev_a);
 	cudaFree(dev_b);
-	cudaFree(dev_r);
 	cudaFree(dev_c);
 }
